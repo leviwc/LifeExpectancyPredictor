c58058a716206864f3ce50bb2b12eda95baee5b1
leviwc <leviwc3@hotmail.com>
2023-06-25 22:02:00 -0300

    feat(MLP) add mlp processing and finish readme

--- README.md
+++ README.md
@@ -1,3 +1,110 @@
 # LifeExpectancyPredictor
-Utilizando World Development Indicators da base the world database https://databank.worldbank.org/source/world-development-indicators/preview/on# esse trabalho utiliza o algoritmo KNN parar prever a expectativa de vida de um pais
+
+Utilizando World Development Indicators da base the world database https://databank.worldbank.org/source/world-development-indicators/preview/on# esse trabalho utiliza o algoritmo KNN, Naive Bayes e MLP parar prever a expectativa de vida de um pais a partir de indicadores de desenvolvimento
+
+
+
+# Pre processing
+
+Para o pré processamento dos dados, temos o csv de cada etapa na pasta prints/
+
+Inicialmente são removidas as colunas desnecessárias 'Country Code', 'Series Name' ficando apenas com o nome do país, código do indicador e anos
+
+Queremos que os indicadores sejam as features, e o indicador de "life expectancy at birth seja a classe a ser predita, então vamos pivotear a base de dados de forma que os indicadores sejam as colunas.
+
+Agora temos uma escolha com prós e contras, se iremos isolar o ano ou utilizar o dataset para todos os anos, formando então indices de pais - ano
+
+O lado positivo de utilizar um ano só é mais dados podendo criar modelos mais robustos, o negativo é aumentar o tempo de processamento e criar dados duplicados.
+Sendo assim fiz o teste com ambas as possibilidades
+
+Agora vamos lidar com os dados nulos, foram testados algumas formas, preenchendo com 0, eliminando todas as linhas sem dados, eliminando todas as colunas com algum dado nulo. E a que teve um sucesso maior foi inicialmente retirar linhas com muitos dados faltando (no caso 14 teve mais sucesso) e após isso eliminar todas as caracteristicas com dados nulos
+
+Também foram retirados todas as linhas com o "life expectancy at birth nulo", porque esses casos geralmente também tinham muitos outros nulos e n teria um beneficio mante-los adicionando uma predição como dado
+
+Agora vamos classificar a coluna de expectativa de vida para facilitar as predições,
+inicialmente foi testado com classes de tamanho de idade 2: (60-62), (63 - 64), ...
+Isso teve resultados de predição muito ruins, com 13% - 18% testando com o metodo leave one out
+Então seguí com classes de tamanho 5 (60 - 64), (65 - 60) obtendo um resultado inicial de em 50% no KNN
+
+Após isso o vetor foi normalizado para facilitar a aplicação nos modelos, utilizando o algoritmo MinMaxScaler ficamos com um dataset tratado, com valores de 0 a 1 e com a classes definidas para o life expectancy at birth
+
+# Aplicando Modelos
+
+Com o dataset tratado foram testados com a técnica leave_one_out para determinar acuracia. Após receber o resultado, eram feitas mudanças no dataset de forma que a acuracia aumente
+
+# KNN
+
+Inicialmente os dados foram tratados com testes seguindo o KNN,
+
+Com o ano 2019 apenas:
+
+que teve acuracia de 13% para classes de tamanho 2, e 50% para classes de tamanho 5
+
+Após as alterações e testes com K de 1 a 100 e melhor resultado foi atingido 23% para classe 2 e 63% para classes de tamanho 5
+
+Com todos os anos:
+
+Com os dados tratados, a accuracia inicial foi de:
+83%, porém isso se deu principalmente pois os dados de um pais mudam muito pouco de um ano para o seguinte, e utilizando o leave one out acaba enviesando o teste
+por isso retirei no treino todas as instancias de um pais quando ele é o escolhido
+
+53% KNN
+
+Após melhorias no processamento:
+
+66%
+
+# Naive Bayes
+Utilizado após dado tratado anteriormente, teve uma acuracia de cerca de 43%
+Foi tentado separar as features em 3 classes para tentar melhor a acuracia mas apenas diminuiu
+
+
+# MLP
+Para 2019 apenas
+Utilizando 100, 100 hidden layers e 1000 max_iteration inicialmente, o resultado foi cerca de 33% inicialmente
+
+Para todos os anos
+Aqui a base de dados cresceu, foi de 266 para 1400, então rodar o leave one out começou a demorar também
+Para tentar otimizar esse processo foi adicionado execução paralela no leave one out utilizando o maximo de recursos
+
+E no fim utilizados apenas uma camada com 14, max_iter 3000, activation= 'Relu'
+
+Com resultado supreendente de 68%!
+
+Tentando com 64 nós, o resultado foi também de 68%
+
+
+# Resultado
+
+classes de idade tamanho 5:
+
+2019:<br/>
+Final K: 21 <br/>
+KNN: 64.13043478260869%<br/>
+Naive Bayes: 42.391304347826086%<br/>
+MLP: 64.67391304347827%<br/>
+
+Todos os anos:<br/>
+Após testar k de 1 a 100 temos:<br/>
+Final K: 9<br/>
+KNN: 66.21717530163235%<br/>
+Naive Bayes: 40.17033356990773%<br/>
+MLP: 68.27537260468416%<br/>
+
+classes de idade tamanho 2:<br/>
+
+2019:<br/>
+Final K: 1<br/>
+KNN: 41.30434782608695%<br/>
+Naive Bayes: 23.369565217391305%<br/>
+MLP: 32.608695652173914%<br/>
+
+Todos os anos:<br/>
+Final K: 13<br/>
+KNN: 38.963804116394606%<br/>
+Naive Bayes: 22.995031937544358%<br/>
+MLP: 35.060326472675655%<br/>
+
+
+
 
--- expectancy.py
+++ expectancy.py
@@ -3,15 +3,17 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from multiprocessing import Pool, freeze_support
 
-pd.set_option("display.max_rows", 20000)
-pd.set_option("display.max_columns", 6)
+pd.set_option("display.max_rows", 6)
+pd.set_option("display.max_columns", 14)
 pd.set_option("display.expand_frame_repr", True)
 pd.set_option('display.width', 1000)
 
 EMPTY_TOKEN = '..'
-
-dataset = pd.read_csv('full_size.csv')
+DATA_FILE = 'world_indicators_database.csv'
+dataset = pd.read_csv(DATA_FILE)
 LIFE_EXPECTANCY_COLUMN = 'SP.DYN.LE00.IN'
 
 # define classes of age as (1,2), (3, 4), (5,6) ...
@@ -36,6 +38,30 @@ def transform_to_class_range_five(age):
 
     return age_class
 
+# pivot dataset and change name
+def pivot_dataset(dataset, col_name):
+    # Pivot to use the indices as columns, new index will be country Name - year
+    dataset_with_of_col = dataset.pivot(index= 'Country Name', columns='Series Code', values=col_name)
+
+    # add col_name in Country Name
+    dataset_with_of_col.index = dataset_with_of_col.index + ' - ' + col_name
+
+    return dataset_with_of_col
+
+# merge all years in one column
+def merge_all_years_and_pivot(dataset):
+    #new dataframe empty with columns series codes and country name as index with no rows
+    columns = dataset['Series Code'].unique()
+    new_dataset = pd.DataFrame(columns= columns)
+
+    for column in dataset.columns:
+        if column not in ['Country Name', 'Series Code']:
+
+            new_dataset = pd.concat([new_dataset, pivot_dataset(dataset, column)], ignore_index=False )
+
+    return new_dataset
+
+
 # Counting empty values by column
 def count_empty_values(dataset):
   empty_amount = {}
@@ -58,15 +84,29 @@ def make_series_dict():
         series_dict[row['Series Code']] = row['Series Name']
     return series_dict
 
-# Remove columns from dataset that aren't Country Name, Series Code, 2019 [YR2019]
+# Remove columns Country Code Series Name
 def remove_columns(dataset):
   for column in dataset.columns:
-    if column not in ['Country Name', 'Series Code', '2019 [YR2019]']:
+    if column in ['Country Code', 'Series Name']:
       dataset.drop(column, axis=1, inplace=True)
   return dataset
 
+# Get training set
+def remove_same_country(train, test):
+    # Extract the Country Name values from the test dataset
+    test_country_names = test.index[0].split(' - ')[0]
+
+    # Use boolean indexing to filter the train dataset
+    training_set = train[~train.index.str.startswith(tuple(test_country_names))]
+
+    # Return the training set
+    return training_set
+
 # get training set
 def get_training_set(train, test):
+    #drop indexes with in train that are share a begining of Country Name of test
+    train = remove_same_country(train, test)
+
     x_train, y_train = train.drop(LIFE_EXPECTANCY_COLUMN, axis=1), train[LIFE_EXPECTANCY_COLUMN]
     x_test, y_test = test.drop(LIFE_EXPECTANCY_COLUMN, axis=1), test[LIFE_EXPECTANCY_COLUMN]
 
@@ -74,7 +114,7 @@ def get_training_set(train, test):
 
 # use knn
 def use_knn(train, test, k):
-
+    # print(test)
     knn = KNeighborsClassifier(n_neighbors=k)
     x_train, y_train, x_test, y_test = get_training_set(train, test)
 
@@ -90,29 +130,16 @@ def leave_one_out_with_knn(normalized_dataset, final_k):
     for country in normalized_dataset.index:
         # normalized_dataset without this country
         normalized_dataset_current = normalized_dataset.drop(country, axis=0)
-        #create dataset with only this country
         current_country = normalized_dataset.filter(items=[country], axis=0)
+        #create dataset with only this country
         knn_result = use_knn(normalized_dataset_current, current_country, final_k)
         sum_got_wright += knn_result[country]
     res = sum_got_wright / sum_total * 100
     return res
 
-def use_nayve_bayes(train, test, k):
+def use_nayve_bayes(train, test):
     # Create a Naive Bayes classifier
     nb_classifier = GaussianNB()
-
-    # Define the bin edges for the three classes
-    bin_edges = [-0.1, 0.3, 0.6, 1.1]
-
-    # Define the labels for the three classes
-    class_labels = [0, 1, 2]
-
-    for column in train.columns:
-        if column == LIFE_EXPECTANCY_COLUMN:
-            continue
-        # Use the cut() function to transform the values into classes
-        train[column] = pd.cut(train[column], bins=bin_edges, labels=class_labels, include_lowest=True)
-
 
     x_train, y_train, x_test, y_test = get_training_set(train, test)
 
@@ -123,8 +150,10 @@ def use_nayve_bayes(train, test, k):
     y_pred = nb_classifier.predict(x_test)
     return y_pred == y_test
 
+
+
 # tests the model leaving one out for every country
-def leave_one_out_with_nayve_bayes(normalized_dataset, final_k):
+def leave_one_out_with_nayve_bayes(normalized_dataset):
     sum_got_wright = 0
     sum_total = len(normalized_dataset.index)
     # make leave one out testing for every country
@@ -133,8 +162,42 @@ def leave_one_out_with_nayve_bayes(normalized_dataset, final_k):
         normalized_dataset_current = normalized_dataset.drop(country, axis=0)
         #create dataset with only this country
         current_country = normalized_dataset.filter(items=[country], axis=0)
-        nayve_bayes_result = use_nayve_bayes(normalized_dataset_current, current_country, final_k)
+        nayve_bayes_result = use_nayve_bayes(normalized_dataset_current, current_country)
         sum_got_wright += nayve_bayes_result[country]
+    res = sum_got_wright / sum_total * 100
+    return res
+
+def use_mlp(args):
+    train, test = args
+    # Create a Naive Bayes classifier
+    nb_classifier = MLPClassifier(hidden_layer_sizes=(14,), max_iter=3000, activation='relu')
+
+    x_train, y_train, x_test, y_test = get_training_set(train, test)
+
+    # Train the classifier using the training data
+    nb_classifier.fit(x_train, y_train)
+
+    # Make predictions on the test data
+    y_pred = nb_classifier.predict(x_test)
+    return y_pred == y_test
+
+# Function to perform leave one out testing in parallel
+def leave_one_out_with_mlp(normalized_dataset):
+    sum_got_wright = 0
+    sum_total = len(normalized_dataset.index)
+
+    # Prepare arguments for parallel processing
+    args = [(normalized_dataset.drop(country, axis=0), normalized_dataset.filter(items=[country], axis=0))
+            for country in normalized_dataset.index]
+
+    # Perform parallel processing using multiprocessing.Pool
+    with Pool() as pool:
+        results = pool.map(use_mlp, args)
+
+    # Calculate the accuracy
+    for res in results:
+        sum_got_wright += res.values[0]
+
     res = sum_got_wright / sum_total * 100
     return res
 
@@ -161,14 +224,20 @@ code_to_name_of_features = make_series_dict()
 
 def process_data(dataset):
 
-    # Remove columns from dataset that aren't Country Name, Series Code, 2019 [YR2019]
-    dataset = remove_columns(dataset)
 
-    # Pivot to use the indices as columns
-    dataset = dataset.pivot(index='Country Name', columns='Series Code', values='2019 [YR2019]')
+    # Remove columns from dataset that aren't Country Name, Series Code, or any year
+    dataset = remove_columns(dataset)
+    dataset.to_csv('prints/after_drop.csv')
+
+    # Pivot to use the indices as columns, new index will be country Name - year
+    dataset = merge_all_years_and_pivot(dataset)
+
+    ## Only 2019
+    # dataset = dataset.pivot(index='Country Name', columns='Series Code', values='2019 [YR2019]')
 
     # remove rows with '..' values in LIFE_EXPECTANCY_COLUMN rows
     dataset = dataset[dataset[LIFE_EXPECTANCY_COLUMN] != EMPTY_TOKEN]
+    dataset.to_csv('prints/after_pivot.csv')
 
     # substitute empty values with null
     dataset.replace(EMPTY_TOKEN, np.nan, inplace=True)
@@ -178,16 +247,14 @@ def process_data(dataset):
 
     # Concatenate the two dataframes
     dataset = reorder_class(dataset)
+    dataset.to_csv('prints/after_class_transformation.csv')
 
-    # Filter rows with more than 15 nulls
-    dataset = dataset[dataset.isnull().sum(axis=1) <= 15]
-
-    # # Fill empty columns that have null values
-    # dataset = dataset.fillna(0.5, axis=1)
-
+    # Filter rows with more than 14 nulls
+    dataset = dataset[dataset.isnull().sum(axis=1) <= 14]
 
     # Drop columns with any null values
     dataset = dataset.dropna(how='any', axis=1)
+    dataset.to_csv('prints/after_class_removing_rows_and_columns_for_null.csv')
 
     columns_to_normalize = dataset.columns.drop(LIFE_EXPECTANCY_COLUMN)
 
@@ -200,17 +267,22 @@ def process_data(dataset):
     # Create a new dataframe with normalized values
     normalized_dataset = pd.DataFrame(dataset, columns=dataset.columns)
 
-    # print(normalized_dataset)
+    normalized_dataset.to_csv('prints/after_normalizing.csv')
 
     return normalized_dataset
 
-dataset = process_data(dataset)
+if __name__ == '__main__':
+    freeze_support()
+    dataset = process_data(dataset)
 
-final_k = 1
-print(final_k)
+    final_k = 9
+    print('Final K: ' + str(final_k))
 
-res = leave_one_out_with_knn(dataset, final_k)
-print(str(res) + '%')
+    res = leave_one_out_with_knn(dataset, final_k)
+    print('KNN: ' + str(res) + '%')
 
-res = leave_one_out_with_nayve_bayes(dataset, final_k)
-print(str(res) + '%')
+    res = leave_one_out_with_nayve_bayes(dataset)
+    print('Naive Bayes: ' + str(res) + '%')
+
+    res = leave_one_out_with_mlp(dataset)
+    print('MLP: ' + str(res) + '%')
